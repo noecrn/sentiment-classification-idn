3f16abd8718af477df1882208f580579b87bb031
noe.cornu <noe.cornu1@gmail.com>
2025-06-10 10:38:03 +0700

    [FIX] merge conflicts

--- src/model.py
+++ src/model.py
@@ -1,5 +1,3 @@
-# src/model.py
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
--- src/predict.py
+++ src/predict.py
@@ -1,20 +1,9 @@
 import pandas as pd
-<<<<<<< HEAD
-from .preprocess import transform_with_features
-
-def predict_and_export(model, vectorizer, test_path, output_path):
-    test_df = pd.read_csv(test_path)
-    
-    # Appliquer le même vectorizer que lors de l'entraînement
-    X_test = transform_with_features(test_df["reviews_content"], vectorizer)
-    
-=======
 
 def predict_and_export(model, vectorizer, test_path, output_path):
     test_df = pd.read_csv(test_path)
     texts = test_df['reviews_content'].apply(lambda x: x.lower())
     X_test = vectorizer.transform(texts)
->>>>>>> parent of baace9a ([ADD] Implement advanced text preprocessing and feature extraction for sentiment analysis)
     preds = model.predict(X_test)
 
     submission = pd.DataFrame({
